689af29ad2c9e91c1f21cd30bd63ebbc4aa2f78e
Magefan - e-Commerce solutions you can trust <ihoronesuch@gmail.com>
2025-08-20 18:39:18 +0300

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -3,15 +3,16 @@
 if [ -d "deploy" ]; then
     echo "Folder 'deploy' already exists."
 else
-    wget https://github.com/magefan/magento2-zero-downtime-deploy/archive/refs/heads/master.zip
-    unzip master.zip
-    rm master.zip
+    wget -O deploy.zip https://github.com/magefan/magento2-zero-downtime-deploy/archive/refs/heads/master.zip
+    unzip deploy.zip
+    rm -f deploy.zip
     mv magento2-zero-downtime-deploy-master deploy
-    rm deploy/installation.sh
-    cp app/etc/env.php deploy/app/env.php
     cp deploy/app/static-content-deploy.sh.sample deploy/app/static-content-deploy.sh
+    cp app/etc/env.php deploy/app/env.php
     echo $'\n'deploy/app/env.php >> .gitignore
     chmod +x deploy/run
+    rm deploy/installation.sh
+    rm zdd_installation.sh
 
     echo "";
     echo "";
