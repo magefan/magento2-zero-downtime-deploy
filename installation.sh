4eb07b6452aec286dd5347f68e63376318a1f783
Magefan - e-Commerce solutions you can trust <ihoronesuch@gmail.com>
2025-08-20 18:30:27 +0300

    Create installation.sh

---
+++ installation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -d "deploy" ]; then
+    echo "Folder 'deploy' already exists."
+else
+    wget https://github.com/magefan/magento2-zero-downtime-deploy/archive/refs/heads/master.zip
+    unzip master.zip
+    rm master.zip
+    mv magento2-zero-downtime-deploy-master deploy
+    cp app/etc/env.php deploy/app/env.php
+    cp deploy/app/static-content-deploy.sh.sample deploy/app/static-content-deploy.sh
+
+    echo "";
+    echo "";
+    echo "Edit deploy/app/env.php file to disconnect it from live session and cache."; 
+    echo "Edit deploy/app/static-content-deploy.sh to set correct static-deploy commands"
+    echo 'To execute deployment, run the command: ./deploy/run'
+fi
