1ba1cb575dab63d09f9a614e70999bbdbc82277e
Magefan - e-Commerce solutions you can trust <ihoronesuch@gmail.com>
2025-08-20 18:34:39 +0300

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -10,6 +10,8 @@ else
     rm deploy/installation.sh
     cp app/etc/env.php deploy/app/env.php
     cp deploy/app/static-content-deploy.sh.sample deploy/app/static-content-deploy.sh
+    echo $'\n'deploy/app/env.php >> .gitignore
+    chmod +x deploy/run
 
     echo "";
     echo "";
